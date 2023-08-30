16918d59789ff70d9accc82753b571e8d9aa0a8b
Thach Ty <nguyenngocthach1506@gmail.com>
2023-08-31 01:06:15 +0700

    Pull admin

--- Controllers/index.js
+++ Controllers/index.js
@@ -26,17 +26,23 @@
 
 /*===========================GLOBAL===========================*/
 var arraySearch = [];
+var listProducts = [];
 /*===========================DOM==============================*/
-const TABLEPRODUCT = document.querySelector('#listProduct');
+const TABLE_PRODUCT = document.querySelector('#listProduct');
+const BODY_CART = document.querySelector('#cartBody');
 
 
 
+
+
+const URL_CART = "https://64c62b5ec853c26efadb28e8.mockapi.io/Cart/";
+const URL_PRODUCTS = "https://64c62b5ec853c26efadb28e8.mockapi.io/Product/";
 /*============================================================*/
-function RenderProduct(listProducts) {
-  TABLEPRODUCT.innerHTML = "";
+function RenderProduct(list) {
+  TABLE_PRODUCT.innerHTML = "";
   var text = "";
 
-  for (const product of listProducts) {
+  for (const product of list) {
     text += `<div class="col">
                 <div class="card">
                     <img src=${product.Image} alt="">
@@ -53,28 +59,37 @@ function RenderProduct(listProducts) {
                         ${product.Desc}
                       </p>
                     </div>
-                    <a href="#" class="btn-Add">
+                    <a onclick="AddCart(${product.Id})" class="btn-Add" role="button">
                         <i class="bi bi-bag-plus-fill"></i>
                     </a>
                 </div>
             </div>`;
-
-
   }
-
-  TABLEPRODUCT.innerHTML = text;
+  TABLE_PRODUCT.innerHTML = text;
 }
 
-async function GetProducts() {
-  //  url tạm thời
-  const URL = "../Product.json";
+async function GetAPI(URL, id = "") {
+  URL += id;
   var respon = await axios.get(URL, {}).then()
   return respon.data;
 }
 
+async function PutAPI(URL, id, data = []) {
+  URL += id;
+  var respon = await axios.put(URL, { Products: data }).then()
+  return respon.request;
+}
+
 document.body.onload = async function () {
-  var products = await GetProducts();
+  // Product
+  var products = await GetAPI(URL_PRODUCTS);
   RenderProduct(products);
+
+  // Cart
+  var prodctFromCart = await GetProductsFromCart("1");
+  listProducts = await GetProductsFromCart();
+  RenderCart(prodctFromCart.reverse());
+
 }
 
 function SearchProducts(list = [], valueSearch = "", key) {
@@ -92,7 +107,7 @@ function SearchProducts(list = [], valueSearch = "", key) {
 
 document.querySelector('#categories').onchange = async function (event) {
   var slect = event.target;
-  var products = await GetProducts();
+  var products = await GetAPI(URL_PRODUCTS);
   if (slect.options.selectedIndex == 0) {
     RenderProduct(products);
   }
@@ -104,7 +119,7 @@ document.querySelector('#categories').onchange = async function (event) {
 
 document.querySelector('#typeSearch').onkeyup = async function (event) {
   var type = event.target;
-  var products = await GetProducts();
+  var products = await GetAPI(URL_PRODUCTS);
   if (type.value == "") {
     RenderProduct(products);
   }
@@ -112,4 +127,150 @@ document.querySelector('#typeSearch').onkeyup = async function (event) {
     var listSearch = SearchProducts(products, type.value, "Name")
     RenderProduct(listSearch);
   }
+}
+
+
+/*==========================CART================================*/
+function RenderCart(list) {
+  document.getElementById('numberItems').innerText = `(${list.length} items)`;
+
+  BODY_CART.innerHTML = "";
+  var text = "";
+
+
+  for (let index = list.length - 1; 0 <= index; index--) {
+    const products = list[index];
+    // const total = products.Price * quantity.value;
+
+    text += `<!-- Cart Item -->
+                <div class="cart-items ">
+                    <div class="cart-img"><img src=${products.Image} alt=""></div>
+                    <div class="cart-detail">
+                        <div class="details">
+                            <p id="nameProduct">${products.Name}</p>
+                            <a class="btn-remove" onclick=RemoveProduct(${products.Id}) role="button">
+                              <i class="bi bi-x"></i>
+                            </a>
+                            <div class="row">
+                                <span class="col">Quantity:</span>
+                                <p id="quantityProduct" data-id=${products.Id} class="col quantity">1</p>
+                            </div>
+                            <div class="row">
+                                <span class="col">Price:</span>
+                                <p id="priceProduct" class="col price">${products.Price}</p>
+                            </div>
+                            <div class="row">
+                                <span class="col">Total</span>
+                                <p id="totalProduct" data-id=${products.Id} class="col total">${products.Price}</p>
+                            </div>
+                        </div>
+                        <div class="cart-quantity">
+                            <button onclick="UpNumber(${products.Id})" class="btn-plus" type="button"><i class="bi bi-plus-circle"></i></button>
+                            <input onchange="ChangeNumber(${products.Id})" data-id=${products.Id} id="numberProduct" type="number" value="1" min=0>
+                            <button onclick="DownNumber(${products.Id})" class="btn-dash" type="button"><i class="bi bi-dash-circle"></i></button>
+                        </div>
+                    </div>
+                </div>`;
+  }
+
+  BODY_CART.innerHTML = text;
+  Total();
+}
+
+async function AddCart(id) {
+  let product = await GetAPI(URL_PRODUCTS, id);
+
+  // Kiểm tra resquest thành công mới add vào danh sách
+  if (true) {
+    AddListProducts(product);
+    RenderCart(listProducts);
+    // Kiểm tra put thành công mới render danh sách cart
+    let respon = await PutAPI(URL_CART, "1", listProducts);
+  }
+}
+
+async function GetProductsFromCart(idCart = "1") {
+  let cart = await GetAPI(URL_CART, idCart);
+  return cart.Products;
+}
+
+
+// Kiểm tra Product có trong listProduct không
+// Nếu CÓ => return
+// KHÔNG => push vào danh sách
+async function AddListProducts(object) {
+
+  for (const iterator of listProducts) {
+    if (iterator.Id == object.Id) {
+      return;
+    }
+  }
+  listProducts.push(object);
+
+}
+
+
+// Remove Product from Cart
+async function RemoveProduct(id) {
+  for (let index = 0; index < listProducts.length; index++) {
+    if (id == listProducts[index].Id) {
+      listProducts.splice(index, 1);
+    }
+  }
+  PutAPI(URL_CART, "1", listProducts);
+  RenderCart(listProducts);
+}
+
+
+window.DownNumber = function (id) {
+  var number = document.querySelector(`input[data-id="${id}"]`);
+  const QUANTITY = document.querySelector(`#quantityProduct[data-id="${id}"]`);
+
+  if (number.value * 1 > 0) {
+    number.value = number.value * 1 - 1;
+    QUANTITY.innerText = number.value;
+    TotalProduct(id);
+  }
+}
+
+window.UpNumber = function (id) {
+  var number = document.querySelector(`input[data-id="${id}"]`);
+  const QUANTITY = document.querySelector(`#quantityProduct[data-id="${id}"]`);
+
+  number.value = number.value * 1 + 1;
+  QUANTITY.innerText = number.value;
+  TotalProduct(id);
+}
+
+window.ChangeNumber = function (id) {
+
+  TotalProduct(id);
+
+}
+
+
+function TotalProduct(id) {
+  const QUANTITY = document.querySelector(`#quantityProduct[data-id="${id}"]`);
+  const TOTAL_PRODUCT = document.querySelector(`#totalProduct[data-id="${id}"]`);
+
+  var price = document.getElementById('priceProduct').innerText * 1;
+  var inputNumber = document.querySelector(`input[data-id="${id}"]`).value * 1;
+
+  if (inputNumber >= 0) {
+    QUANTITY.innerText = inputNumber;
+    TOTAL_PRODUCT.innerText = (price * inputNumber);
+    Total();
+  }
+}
+
+function Total() {
+  const TOTAL = document.getElementById('total');
+  const TOTAL_PRODUCTS = document.querySelectorAll('.total');
+  var sum = 0;
+
+  for (const iterator of TOTAL_PRODUCTS) {
+    sum += new Number(iterator.innerText);
+  }
+
+  TOTAL.innerText = sum.toLocaleString() + "$";
 }
